224c42dc50792e5be32b772be4ba8e90e729eb5f
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-22 22:34:58 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+int InputInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    if(int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Only integer pls");
+    return 0;
+}
+
+int Exponentiation(int num, int num1)
+{
+    int exponent = 1;
+    for (int i = 1; i <= num1; i++)
+    {
+        exponent *= num;
+    }
+    return exponent;
+}
+
+int firstNumber = InputInt("Enter first number:");
+int secondNumber = InputInt("Enter second number:");
+System.Console.WriteLine(Exponentiation(firstNumber, secondNumber));
