772a55b2f3dc763659b0c187a30c1f2554569bed
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-23 13:44:23 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+
+int InputInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    if (int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Only integer pls");
+    return 0;
+}
+
+int Counter(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int inputNumber = InputInt("Input number");
+System.Console.WriteLine($"Ammount of all digits in {inputNumber} is equal {Counter(inputNumber)}");
